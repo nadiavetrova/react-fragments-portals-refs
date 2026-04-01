501cc91ef04ae1ad497e20505e2cc293d2ee4cd9
Nadezhda <nadi@MacBook-Pro-Nadezhda.local>
2026-04-01 18:45:58 +0800

    Использование Портала

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,15 @@
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
+import React, { Fragment } from "react";
+import ReactDom from "react-dom";
 
-const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onCloseModal}></div>
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onCloseModal}></div>
+}
 
-      <Card className={styles.modal}>
+const Modal = (props) => {
+       return <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
         </header>
@@ -18,7 +20,13 @@ const ErrorModal = (props) => {
           <Button onClick={props.onCloseModal}>Закрыть</Button>
         </footer>
       </Card>
-    </div>
+}
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+     {ReactDom.createPortal(<Backdrop onCloseModal={props.onCloseModal}/>, document.getElementById('backdrop'))}
+    </Fragment>
   );
 };
 
